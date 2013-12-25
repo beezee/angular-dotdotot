3a3af670237cb982f7ec6b86b78a8bdd96893585
Brian <brian@brian-1015E.(none)>
2013-12-24 20:09:50 -0500

    whoops!

--- js/angular-dotdotdot.js
+++ js/angular-dotdotdot.js
@@ -2,8 +2,6 @@ angular.module('dotdotdot-angular', [])
 	.directive('dotdotdot', function(){
 		return {
 			restrict: 'A',
-			scope: false,
-			scope: true,
 			link: function(scope, element, attributes) {
 				scope.$watch(function() {
 					element.dotdotdot({watch: true});
